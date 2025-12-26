5a3c43e271cbc37114b28f5cc36a95455c8a3f27
Shubham Kamble <shubhamkamble3011@gmail.com>
2025-12-26 19:17:09 +0530

    small change

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const blockwidth = 50;
 
 const cols = Math.floor(board.clientWidth / blockwidth);
 const rows = Math.floor(board.clientHeight / blockheight);
+let intervalId=null;
+let food={x:Math.floor(Math.random()*rows),y:Math.floor(Math.random()*cols)}
 
 const blocks = [];
 
@@ -28,21 +30,14 @@ for (let row = 0; row < rows; row++) {
         block.classList.add("block")
         board.appendChild(block)
         blocks[`${row}-${col}`] = block;
-        block.innerText=`${row}-${col}`
+        // block.innerText=`${row}-${col}`
     }
 }
 
 function render(){
-    snake.forEach(segment=>{
-      blocks[`${segment.x}-${segment.y}`].classList.add("fill")
-    })
-}
+     let head=null;
 
-
-
-setInterval(()=>{
-  
-    let head=null;
+     blocks[`${food.x}-${food.y}`].classList.add("food")
 
     if(direction==="left"){
         head={x:snake[0].x, y:snake[0].y-1}
@@ -52,9 +47,23 @@ setInterval(()=>{
     else if(direction==="down"){
         head={x:snake[0].x+1, y:snake[0].y}
     }
-    else if(direction==="down"){
+    else if(direction==="up"){
         head={x:snake[0].x-1, y:snake[0].y}
     }
+
+    if(head.x<0 || head.x>=rows || head.y<0 || head.y>=cols){
+        alert("Game Over")
+        clearInterval(intervalId)
+    }
+
+    if(head.x==food.x && head.y==food.y){
+        blocks[`${food.x}-${food.y}`].classList.remove("food");
+        food={x:Math.floor(Math.random()*rows),y:Math.floor(Math.random()*cols)}
+         blocks[`${food.x}-${food.y}`].classList.add("food");
+         snake.unshift(head)
+    }
+
+
 
     snake.forEach(segment=>{
       blocks[`${segment.x}-${segment.y}`].classList.remove("fill")
@@ -62,19 +71,26 @@ setInterval(()=>{
 
     snake.unshift(head)
     snake.pop()
+    snake.forEach(segment=>{
+      blocks[`${segment.x}-${segment.y}`].classList.add("fill")
+    })
+}
 
-  render()
-},400)
 
-addEventListener('keydown',(event)=>{
-    if(event.key=="ArrowUp"){
-        direction="up"
-    }else if(event.key=="ArrowRight"){
-        direction="right"
-    }else if(event.key=="ArrowLeft"){
-        direction="left"
-    }
-    else if(event.key=="ArrowDown"){
-        direction="down"
-    }
-})
+
+// intervalId=setInterval(()=>{
+//   render()
+// },400)
+
+// addEventListener('keydown',(event)=>{
+//     if(event.key=="ArrowUp"){
+//         direction="up"
+//     }else if(event.key=="ArrowRight"){
+//         direction="right"
+//     }else if(event.key=="ArrowLeft"){
+//         direction="left"
+//     }
+//     else if(event.key=="ArrowDown"){
+//         direction="down"
+//     }
+// })
